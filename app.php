367c10f20767c07455390a03932662b7bfa2567f
shibdib <supernfadmin@gmail.com>
2018-03-16 03:26:01 -0400

    fix rename again?

--- app.php
+++ app.php
@@ -93,7 +93,7 @@ $app->get('/auth/', function () use ($app, $config, $log) {
 
         //Make sure bots nick is set
         if (isset($config['discord']['botNick'])) {
-            $restcord->guild->modifyCurrentUsersNick(['guild.id' => (int)$config['discord']['guildId'], 'nick' => $config['discord']['botNick']]);
+            $restcord->guild->modifyCurrentUserNick(['guild.id' => (int)$config['discord']['guildId'], 'nick' => $config['discord']['botNick']]);
         }
 
         $tokenURL = 'https://login.eveonline.com/oauth/token';

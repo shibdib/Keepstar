a8dfbf626779b8219e57f788fd3805bbba6c7498
H.-Peter Pfeufer <info@ppfeufer.de>
2018-03-25 11:23:44 +0200

    Variables are indeed case sensitive .... Signed-off-by:H.-Peter Pfeufer <info@ppfeufer.de>

--- cron.php
+++ cron.php
@@ -309,7 +309,7 @@ foreach(array_chunk($users, 5, true) as $userSet) {
 
 				foreach($roles as $role) {
 					if($role->name === $authGroup['role']) {
-						if((!in_array($characterData['corporation_id'], $id) && !in_array($characterData['alliance_id'], $id) && !in_array($characterId, $id) && !in_array('1234', $id)) && in_array($role->id, $member->roles)) {
+						if(((isset($characterData['corporation_id']) && !in_array($characterData['corporation_id'], $id)) && (isset($characterData['alliance_id']) && !in_array($characterData['alliance_id'], $id)) && !in_array($characterID, $id) && !in_array('1234', $id)) && in_array($role->id, $member->roles)) {
 							$removeTheseRoles[] = (int) $role->id;
 
 							if((int) $config['discord']['logChannel'] !== 0) {
